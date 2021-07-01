d700eb44da2bf99c576a9983d0b9e38321cafbe3
syangtu <hffbe0f0o@gmail.com>
2021-07-01 01:09:10 +0000

    modified fizzbuzz

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,22 +1,20 @@
-def fizzbuzz
-    num_max = 100
+def fizzbuzz(num)
     
-    (1..num_max).each do |num|
-    
-    if num%3 == 0
-        puts 'Fizz'
-        
-    elsif num%5 == 0 
-        puts 'Buzz'
-        
-    elsif num%15 == 0
+    if num%3 == 0 && num%5 == 0
         puts 'FizzBuzz'
         
+    elsif num%3 == 0
+        puts 'Fizz'
+   
+    elsif num%5 == 0 
+        puts 'Buzz'
+    
     else 
         puts num
     end
-  end    
 end
 
-fizzbuzz
-
+num_max = 100
+(1..num_max).each do |num|
+    fizzbuzz(num)
+end    
